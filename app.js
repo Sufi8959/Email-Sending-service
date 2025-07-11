c23b04e28ee5a525e09d66cc53a3503f39bb6b69
SUFIYAAN\smans <smansoori0707@gmail.com>
2025-07-11 12:45:17 +0530

    updated path

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from "express";
 const app = express();
-import emailRouter from "./routes/email.routes.js";
+import emailRouter from "./src/routes/email.routes.js";
 
 app.use(express.json());
 app.use("/api/v1/email", emailRouter);
